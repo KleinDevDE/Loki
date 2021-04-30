e487e700c28844d7948d1e04b019a0712a73d821
KleinDev <kontakt@kleindev.de>
2021-04-30 10:56:22 +0200

    Reformat MessageTools.java

--- src/main/java/de/kleindev/loki/utils/MessageTools.java
+++ src/main/java/de/kleindev/loki/utils/MessageTools.java
@@ -1,7 +1,6 @@
 package de.kleindev.loki.utils;
 
 import de.kleindev.loki.Loki;
-import de.kleindev.loki.logging.LogType;
 import de.kleindev.loki.logging.Logger;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
@@ -16,24 +15,24 @@ public class MessageTools {
             while (true) {
                 try {
                     Thread.sleep(5184000);
-                    Logger.log(LogType.DEBUG, "Clearing \"messagesWhoDontNeedToBeLogged\" list...");
+                    Logger.debug("Clearing \"messagesWhoDontNeedToBeLogged\" list...");
                     messagesWhoDontNeedToBeLogged.clear();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
-        }).start();
-    }
-
-    public static void deleteMessageLater(TextChannel channel, long messageID, long seconds) {
-        if (seconds <= -1)
+		}).start();
+	}
+	
+    public static void deleteMessageLater(TextChannel channel, long messageID, long seconds){
+        if(seconds <= -1)
             return;
         messagesWhoDontNeedToBeLogged.add(messageID);
         new Thread(() -> {
             try {
-                if (channel.getId() == 585562006023569418L)
+                if(channel.getId() == 585562006023569418L)
                     return;
-                Thread.sleep(1000 * seconds);
+                Thread.sleep(1000*seconds);
                 Loki.getInstance().getDiscordApi().getMessageById(messageID, channel).join().delete();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -41,13 +40,13 @@ public class MessageTools {
         }).start();
     }
 
-    public static void deleteMessageLater(Message message, long seconds) {
-        if (seconds <= -1)
+    public static void deleteMessageLater(Message message, long seconds){
+        if(seconds <= -1)
             return;
         messagesWhoDontNeedToBeLogged.add(message.getId());
         new Thread(() -> {
             try {
-                Thread.sleep(1000 * seconds);
+                Thread.sleep(1000*seconds);
                 message.getMessageDeleteListeners().forEach(message::removeMessageAttachableListener);
                 message.getMessageEditListeners().forEach(message::removeMessageAttachableListener);
                 message.getCachedMessagePinListeners().forEach(message::removeMessageAttachableListener);
@@ -62,31 +61,42 @@ public class MessageTools {
         }).start();
     }
 
-    public static String getMessageURL(Message message) {
-        return "https.//discordapp.com/" + message.getServer().get().getId() + "/" + message.getChannel().getId() + "/" + message.getIdAsString();
+    public static String getMessageURL(Message message){
+        // Server https://discord.com/channels/787468489484271640/829113809054072882/835284423729217556
+        // Private https://discord.com/channels/@me/792847931341537310/835474579504889866
+
+        if (message.isServerMessage()){
+            return "https://discord.com/channels/"+message.getServer().get().getId()+"/"+message.getChannel().getId()+"/"+message.getIdAsString();
+        } else {
+            return "https://discord.com/channels/@me/"+message.getChannel().getId()+"/"+message.getIdAsString();
+        }
     }
 
+    public static Message getMessageByURL(String url){
+        return null;
+    }
+    
     public static void preventMessageFromDeleteLogging(long messageID) {
-        messagesWhoDontNeedToBeLogged.add(messageID);
+    	messagesWhoDontNeedToBeLogged.add(messageID);
     }
-
+    
     public static void preventMessageFromDeleteLogging(Message message) {
-        preventMessageFromDeleteLogging(message.getId());
+    	preventMessageFromDeleteLogging(message.getId());
     }
-
+    
     public static boolean isMessageDeletionIgnored(long messageID) {
-        return messagesWhoDontNeedToBeLogged.contains(messageID);
+    	return messagesWhoDontNeedToBeLogged.contains(messageID);
     }
-
+    
     public static boolean isMessageDeletionIgnored(Message message) {
-        return isMessageDeletionIgnored(message.getId());
+    	return isMessageDeletionIgnored(message.getId());
     }
-
+    
     public static void removeMessageFromDeletionLogging(long messageID) {
-        messagesWhoDontNeedToBeLogged.remove(messageID);
+    	messagesWhoDontNeedToBeLogged.remove(messageID);
     }
-
+    
     public static void removeMessageFromDeletionLogging(Message message) {
-        removeMessageFromDeletionLogging(message.getId());
+    	removeMessageFromDeletionLogging(message.getId());
     }
 }
